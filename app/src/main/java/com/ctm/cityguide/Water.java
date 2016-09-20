900a841a216a18d5a9f93f0010f41d3e9704cd5c
StevesRepository <sjdamico203@sbcglobal.net>
2016-09-20 15:37:43 -0500

    Initial commit

---
+++ app/src/main/java/com/ctm/cityguide/Water.java
@@ -0,0 +1,13 @@
+package com.ctm.cityguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Water extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water);
+    }
+}
